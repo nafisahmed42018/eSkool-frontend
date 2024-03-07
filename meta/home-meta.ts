c683ceb3a969274ee1f7abf623ad0802efd81744
Nafis Ahmed <nafisahmed@iut-dhaka.edu>
2024-03-07 19:50:24 +0600

    setup completed; hero, header and footer designed

---
+++ meta/home-meta.ts
@@ -0,0 +1,12 @@
+interface IMeta {
+  title: string
+  description: string
+  keywords: string
+}
+
+export const HomeMeta: IMeta = {
+  title: 'e-Skool',
+  description:
+    'e-Skool is a platform for students to learn and get help from teachers',
+  keywords: 'Prograaming,MERN,Redux,Machine Learning',
+}
